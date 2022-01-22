93a1800f47327696b9b4cdaed7434d076bc6fc2e
Nicolas Olivain <nicolasolivain@live.com>
2022-01-22 11:44:12 +0100

    structure

---
+++ Components/VisualTranformer.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+
+
+class VisualTransformer(nn.Module):
+    def __init__(self):
+        super(VisualTransformer, self).__init__()
+
+    def forward(self, x):
+        pass
