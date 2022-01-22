93741d0c3d7f0f7ae5ea2cdf047868fb1876005f
Nicolas Olivain <nicolasolivain@live.com>
2022-01-22 14:40:54 +0100

    refactor

---
+++ Components/Tranformer.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+
+from Components.Attention import MultiHeadSelfAttentionL2
+
+
+class Transformer(nn.Module):
+    def __init__(self):
+        super(Transformer, self).__init__()
+
+    def forward(self, x):
+        pass
