8189f1e471181f2fb557eefbf1747d99a68c84af
Nicolas Olivain <nicolasolivain@live.com>
2022-01-29 18:21:31 +0100

    docstring

--- Core/Generator.py
+++ Core/Generator.py
@@ -8,7 +8,17 @@ from Components.SIREN import SIREN
 
 
 class Generator(nn.Module):
-    def __init__(self, lattent_size, img_size, n_channels, feature_hidden_size,  n_transformer_layers=1, mapping_mlp_params=None, transformer_params=None, output_net_params=None):
+    def __init__(self, lattent_size, img_size, n_channels, feature_hidden_size,  n_transformer_layers=1, mapping_mlp_params=None, transformer_params=None):
+        """
+        ViT Generator Class
+        :param lattent_size: number of features in the lattent space
+        :param img_size: output images size, the image will be square sized
+        :param n_channels: number of channel in the output images
+        :param feature_hidden_size: number of features in the transformers and output layers
+        :param n_transformer_layers: number of stacked transformer blocks
+        :param mapping_mlp_params: kwargs for optional parameters of the mapping MLP, mandatory args will be filled automatically
+        :param transformer_params: kwargs for optional parameters of the Transformer blocks, mandatory args will be filled automatically
+        """
         super(Generator, self).__init__()
 
         self.lattent_size          = lattent_size
@@ -19,7 +29,6 @@ class Generator(nn.Module):
 
         self.mapping_params        = {} if mapping_mlp_params is None else mapping_mlp_params
         self.transformer_params    = {} if transformer_params is None else transformer_params
-        self.output_net_params     = {} if output_net_params is None else output_net_params
 
         self.mapping_params['in_features'], self.mapping_params['out_features'] = self.lattent_size, self.img_size*self.feature_hidden_size
         self.mapping_mlp = MLP(**self.mapping_params)
@@ -32,8 +41,8 @@ class Generator(nn.Module):
         self.sln = SLN(self.feature_hidden_size)
 
         self.output_net = nn.Sequential(
-            SIREN(self.feature_hidden_size, 2*img_size, is_first=True),
-            SIREN(2*self.img_size, self.img_size*self.n_channels)
+            SIREN(self.feature_hidden_size, 2*feature_hidden_size, is_first=True),
+            SIREN(2*self.feature_hidden_size, self.n_channels*self.img_size, is_first=False)
         )
 
     def forward(self, x):
@@ -48,6 +57,6 @@ class Generator(nn.Module):
 
 if __name__ == '__main__':
     ipt = torch.randn(10, 1024)
-    mod = Generator(1024, 64, 3, 384)
+    mod = Generator(1024, 64, 3, 200)
     ret = mod.forward(ipt)
     print(ret.shape)
